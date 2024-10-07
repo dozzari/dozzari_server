f2468ca8be800d1a992d2539596fd1ee130bd3de
kiryanchi <kiryanchi@gmail.com>
2024-10-07 22:06:39 +0900

    Fix: CORS

--- src/main/java/today/dozzari/server/global/security/config/SecurityConfig.java
+++ src/main/java/today/dozzari/server/global/security/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
 
